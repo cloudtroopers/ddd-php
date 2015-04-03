5a217f68cad13cc0c196b586e7483d1b3b0373e1
Georgiana Gligor <g@lolaent.com>
2015-04-03 16:33:04 +0300

    Entity and ValueObject interfaces

---
+++ src/Cti/Ddd/Entity.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the DDD for PHP.
+ *
+ * (c) Cloudtroopers <office@cloudtroopers.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cti\Ddd;
+
+interface Entity
+{
+    public function sameIdentityAs($other);
+}
